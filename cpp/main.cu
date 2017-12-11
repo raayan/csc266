b915a57bff23f88289eee67ca326baf03cb09253
RAAYAN A PILLAI <rpillai3@bh25fen.circ.rochester.edu>
2017-12-11 10:59:09 -0500

    Finished up GPU version

--- cpp/main.cu
+++ cpp/main.cu
@@ -7,8 +7,13 @@
 #include <vector>
 #include <math.h>
 // CUDA imports
+#include <curand.h>
+#include <curand_kernel.h>
 #include "helper_cuda.h"
 #include "helper_string.h"
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/sort.h>
 // Library imports
 #include <curl/curl.h>
 #include "json.hpp"
@@ -23,12 +28,12 @@ public:
 	void *operator new(size_t len) {
 		void *ptr;
 		cudaMallocManaged(&ptr, len);
-		cudaDeviceSynchronize();
+//		cudaDeviceSynchronize();
 		return ptr;
 	}
 
 	void operator delete(void *ptr) {
-		cudaDeviceSynchronize();
+//		cudaDeviceSynchronize();
 		cudaFree(ptr);
 	}
 };
@@ -38,7 +43,12 @@ public:
 	double price;
 	double quantity;
 	
-	Order(double p, double q) {
+	__host__ __device__ Order() {
+		price = 0;
+		quantity = 0;
+	}
+	
+	__host__ __device__ Order(double p, double q) {
 		price = p;
 		quantity = q;
 	}
@@ -54,49 +64,53 @@ public:
 
 class MarketBook : public Managed {
 public:
-	double mean;
-	double stdd;
-	vector<Order> asks;
-	vector<Order> bids;
+	int size = 100;
+	Order asks[100];
+	Order bids[100];
 
 	MarketBook() {
-
+		cudaMallocManaged((void**)&asks, size*sizeof(Order));
+		cudaMallocManaged((void**)&bids, size*sizeof(Order));
+	}
+	__device__ MarketBook(int a) {
 	}
 	
-	double calculate_mean() {
+	__host__ __device__ double calculate_mean() {
 		double total_quantity = 0;
 		double total = 0;
-		for(std::vector<Order>::size_type i = 0; i != asks.size(); i++) {
+		for(int i = 0; i != size; i++) {
 			total_quantity += asks[i].quantity;
 			total += asks[i].quantity*asks[i].price;
 		}
-		for(std::vector<Order>::size_type i = 0; i != bids.size(); i++) {
+		for(int i = 0; i != size; i++) {
 			total_quantity += bids[i].quantity;
 			total += bids[i].quantity*bids[i].price;
 		}
 		return total/total_quantity;
 	}
 
-	double calculate_total() {
-		double total = 0;
-		for(std::vector<Order>::size_type i = 0; i != asks.size(); i++) {
-			total += asks[i].quantity;
+
+	__host__ __device__ double calculate_total() {
+		double total_quantity = 0;
+		for(int i = 0; i != size; i++) {
+			total_quantity += asks[i].quantity;
 		}
-		for(std::vector<Order>::size_type i = 0; i != bids.size(); i++) {
-			total += bids[i].quantity;
+		for(int i = 0; i != size; i++) {
+			total_quantity += bids[i].quantity;
 		}
-		return total;
+		return total_quantity;
 	}
 	
-	double calculate_stddev() {
+	__host__ __device__ double calculate_stddev() {
 		double total = calculate_total();
 		double sum = 0;
 		double probability = 0;
-		for(std::vector<Order>::size_type i = 0; i != asks.size(); i++) {
+
+		for(int i = 0; i != size; i++) {
 			probability = asks[i].quantity/total;
 			sum += asks[i].price*asks[i].price*probability;
 		}
-		for(std::vector<Order>::size_type i = 0; i != bids.size(); i++) {
+		for(int i = 0; i != size; i++) {
 			probability = bids[i].quantity/total;
 			sum += bids[i].price*bids[i].price*probability;
 		}
@@ -104,80 +118,160 @@ public:
 		return pow((sum - mean*mean), 0.5);
 	}
 	
-	// ask=0 for bid ask=1 for ask
-	bool insert_order(Order active_order, bool ask) {
+	__host__ __device__ int find_ask(double price) {
+		for(int i=0; i < size-1; i++) {
+			if(asks[i].price <= price) {
+				if(asks[i+1].price > price) {
+					return i;
+				} 
+			}
+		}
+		return -1;
+	}
+
+	__host__ __device__ int find_bid(double price) {
+		for(int i=0; i < size-1; i++) {
+			if(bids[i].price >= price) {
+				if(bids[i+1].price < price) {
+					return i;
+				} 
+			}
+		}
+		return -1;
+	}
+	
+	__host__ __device__ int index_ask(double price) {
+		for(int i=0; i < size-1; i++) {
+			if(asks[i].price == price) return i;
+		}
+		return -1;
+	}
+	__host__ __device__ int index_bid(double price) {
+		for(int i=0; i < size-1; i++) {
+			if(bids[i].price == price) return i;
+		}
+		return -1;
+	}	
+
+	__host__ __device__ bool remove_ask(double price) {
+		int pos = find_ask(price);
+		if(pos == -1) return false;
+		for(int i=pos; i < size-1; i++) {
+			asks[i].price = asks[i+1].price;
+			asks[i].quantity = asks[i+1].quantity;
+		}
+		return true;	
+	}
+
+	__host__ __device__ bool remove_bid(double price) {
+		int pos = find_bid(price);
+		if(pos == -1) return false;
+		for(int i=pos; i < size-1; i++) {
+			bids[i].price = bids[i+1].price;
+			bids[i].quantity = bids[i+1].quantity;
+		}
+		return true;	
+	}
+
+	__host__ __device__ bool insert_ask(Order ask) {
+		int i = 0; 
+		for(int j=0; j<size-1; j++) {
+			if(asks[j].price != 0 && asks[j+1].price ==0) {
+				i = j;
+			}
+		}
+		while ((i > 0) && (ask.price < asks[i-1].price)) {   
+			asks[i].price = asks[i-1].price;
+			asks[i].quantity = asks[i-1].quantity;
+			i = i - 1;
+		}
+		asks[i].price = ask.price;
+		asks[i].quantity = ask.quantity;
+		return true;
+	}
+
+	__host__ __device__ bool insert_bid(Order bid) {
+		int i = 0; 
+		for(int j=0; j<size-1; j++) {
+			if(bids[j].price != 0 && bids[j+1].price ==0) {
+				i = j;
+			}
+		}
+		while ((i > 0) && (bid.price < bids[i-1].price)) {   
+			bids[i].price = bids[i-1].price;
+			bids[i].quantity = bids[i-1].quantity;
+			i = i - 1;
+		}
+		bids[i].price = bid.price;
+		bids[i].quantity = bid.quantity;
+		return true;
+	}
+
+
+
+	__host__ __device__ bool insert_order(Order active_order, bool ask) {
 		double remaining = 0;
 		int pos = 0;
 		if(ask) {
-			pos = (lower_bound(bids.begin(), bids.end(), active_order) - bids.begin());
-			if(pos < bids.size()) {
-				if(bids[pos].price >= active_order.price && active_order.quantity > bids[pos].quantity) {
+			pos = find_bid(active_order.price);
+			if(pos != -1) {
+				if(active_order.quantity > bids[pos].quantity) {
 					active_order.quantity -= bids[pos].quantity;
-					bids.erase(bids.begin()+pos);
+					remove_bid(bids[pos].price);
 					return insert_order(active_order, ask);
 				}else{
 					bids[pos].quantity -= active_order.quantity;
 					return true;
 				}
 			} else {
-				pos = (lower_bound(asks.begin(), asks.end(), active_order) - asks.begin());
-				if(asks[pos].price == active_order.price) {
+				pos = index_ask(active_order.price);
+				if(pos != -1) {
 					asks[pos].quantity += active_order.quantity;
 				} else {
-					asks.insert(asks.begin()+pos, active_order);
+					insert_ask(active_order);
 				}
 				return false;
 			}
 		} else {
-			pos = (lower_bound(asks.begin(), asks.end(), active_order) - asks.begin());
-			if(asks[pos].price <= active_order.price && pos < asks.size()) {
+			pos = find_ask(active_order.price);
+			if(pos != -1) {
 				if(active_order.quantity > asks[pos].quantity) {
 					active_order.quantity -= asks[pos].quantity;
-					asks.erase(asks.begin()+pos);
+					remove_ask(asks[pos].price);
 					return insert_order(active_order, ask);
 				}else{
 					asks[pos].quantity -= active_order.quantity;
 					return true;
 				}
 			} else {
-				pos = (lower_bound(bids.begin(), bids.end(), active_order) - bids.begin());
-				if(bids[pos].price == active_order.price) {
+				pos = index_bid(active_order.price);
+				if(pos != -1) {
 					bids[pos].quantity += active_order.quantity;
 				} else {
-					bids.insert(bids.begin()+pos, active_order);
+					insert_bid(active_order);
 				} 
 				return false;
 			}
 		}
 	}
+	
+/*
+	__host__ __device__ bool sort_books() {
+		sort(asks.begin(), asks.end());
+		sort(bids.begin(), bids.end());
+		return true;
+	}
+*/
+	__device__ void copy_in(MarketBook book) {
+		for(int i=0; i<size; i++) {
+			asks[i].price = book.asks[i].price;
+			asks[i].quantity = book.asks[i].quantity;
+			bids[i].price = book.bids[i].price;
+			bids[i].quantity = book.bids[i].quantity;
+		}
+	}
 };
 
-std::ostream& operator<< (std::ostream & out, Order const& data) {
-	out << "Order(";
-	out << "price=";
-	out << data.price;
-	out << ", ";
-	out << "quantity=";
-	out << data.quantity;
-	out << ")";
-	return out ;
-}
-std::ostream& operator<< (std::ostream & out, MarketBook const& data) {
-	out << "MarketBook(";
-	out << "asks=[\n";
-	for(std::vector<Order>::size_type i = 0; i != data.asks.size(); i++) {
-		out << data.asks[i];
-		if(i < data.asks.size()-1) out << "\n";
-	}
-	out << "]\n";
-	out << "bids=[\n";
-	for(std::vector<Order>::size_type i = 0; i != data.bids.size(); i++) {
-		out << data.bids[i];
-		if(i < data.bids.size()-1) out << "\n";
-	}
-	out << "])";
-	return out ;
-}
 
 /////////////////////////// CUDA stuff: ///////////////////////////
 #define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
@@ -190,13 +284,32 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=t
 	}
 }
 
-__device__ int convolve() {
-	return 1;
+__global__ void setup_kernel(curandState *state) {
+	int id = threadIdx.x + blockIdx.x * blockDim.x;
+	curand_init(1234, id, 0, &state[id]);
 }
 
-__global__ void convolve_gpu(int blocks, int blocksize) {
-	int tid = blockIdx.x * blockDim.x + threadIdx.x;
-	int thread_count = blocks*blocksize;
+
+__global__ void simulate_market(MarketBook in_book, int generations, curandState *state, double *means, double *devs) {
+	int id = threadIdx.x + blockIdx.x * blockDim.x;
+	MarketBook book = MarketBook(0);
+	book.copy_in(in_book);
+	curand_init(1234, id, 0, &state[id]);
+	curandState localState = state[id];
+
+	double gen_mean, gen_stddev, number;
+	for(int i=0; i<generations; i++) {
+		gen_mean = book.calculate_mean();
+		gen_stddev = book.calculate_stddev();
+		number = curand_log_normal_double(&localState, gen_mean, gen_stddev)-1;
+		if(curand_uniform(&localState) > 0.5) {
+			book.insert_order(Order(number, 1), false);
+		} else {
+			book.insert_order(Order(number, 1), true);
+		}
+	}
+	means[id] = gen_mean;
+	devs[id] = gen_stddev;
 }
 
 
@@ -207,10 +320,9 @@ static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *use
 	return size * nmemb;
 }
 
-
 int main(int argc, char *argv[]) {
-	if(argc != 3) {
-		fprintf(stderr, "Usage: %s gpu generations\n", argv[0]);
+	if(argc != 5) {
+		fprintf(stderr, "Usage: %s gpu trades generations blocksize\n", argv[0]);
 		fprintf(stderr, "gpu=0 for cpu\n");
 		exit(1);
 	}
@@ -218,14 +330,14 @@ int main(int argc, char *argv[]) {
 
 // Get the current order book
 	string baseURL = "https://api.bitfinex.com/v1";
-	string type = "book";
-	string symbol = "ltcbtc";
-	string URL = baseURL + "/" + type + "/" + symbol;
 	CURL *curl;
 	CURLcode res;
 	string readBuffer;
 	json data;
 
+	string type = "stats";
+	string symbol = "ltcbtc";
+	string URL = baseURL + "/" + type + "/" + symbol;
 	curl = curl_easy_init();
 	if(curl) {
 		curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
@@ -236,41 +348,57 @@ int main(int argc, char *argv[]) {
 		data = json::parse(readBuffer.c_str());
 	}
 
+	double volume24 = atof(data[0]["volume"].get<string>().c_str());
+
+	type = "book";
+	URL = baseURL + "/" + type + "/" + symbol;
+	curl = curl_easy_init();
+	string readBuffer2;
+	if(curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, URL.c_str());
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer2);
+		res = curl_easy_perform(curl);
+		curl_easy_cleanup(curl);
+		data = json::parse(readBuffer2.c_str());
+	}
+
+	int trades = atoi(argv[2]);
+	int generations = atoi(argv[3]);
+	int blocksize = atoi(argv[4]);
+
 	json obj;
 	double amount, price;
 	MarketBook book;
+
 
 	for(int i=0; i<data["bids"].size(); i++){
 		obj = data["bids"][i];
 		amount = atof(obj["amount"].get<string>().c_str());
 		price = atof(obj["price"].get<string>().c_str());
-		book.bids.push_back(Order(price, amount));
+		book.bids[i] = Order(price, amount);
 	}
 	for(int i=0; i<data["asks"].size(); i++){
 		obj = data["asks"][i];
 		amount = atof(obj["amount"].get<string>().c_str());
 		price = atof(obj["price"].get<string>().c_str());
-		book.asks.push_back(Order(price, amount));
+		book.asks[i] = Order(price, amount);
 	}
 
-	cout << book.calculate_mean() << endl;
-	cout << book.calculate_total() << endl;
-	cout << book.calculate_stddev() << endl;
-	sort(book.asks.begin(), book.asks.end());
-	sort(book.bids.begin(), book.bids.end());
-	int generations = atoi(argv[2]);
+	printf("[b] mean: %f, std-dev: %f\n", book.calculate_mean(), book.calculate_stddev());
 
 	if(atoi(argv[1]) == 0) {
 	// CPU
 		default_random_engine generator;
-		
-		for(int i=0; i<10; i++) {
-			double gen_mean = book.calculate_mean();
-			double gen_stddev = book.calculate_stddev();
+		double gen_mean, gen_stddev, number;
+		for(int i=0; i<generations; i++) {
+			gen_mean = book.calculate_mean();
+			gen_stddev = book.calculate_stddev();
 			normal_distribution<double> distribution(gen_mean, gen_stddev);
-			double number = distribution(generator);
-			cout << number << endl;
+			number = distribution(generator);
+			book.insert_order(Order(number, 1), true);
 		}
+//		cout << book.calculate_total() << ", " << book.calculate_mean() << ", " << book.calculate_stddev() << endl;
 
 
 	} else {
@@ -288,76 +416,45 @@ int main(int argc, char *argv[]) {
 		devID = gpuGetMaxGflopsDeviceId();
 		checkCudaErrors(cudaSetDevice(devID));
 
-	/*
-	// COPY TO GPU
-	// create GPU ppm
-		cudaMalloc((void **) &d_img_r, size * sizeof(*d_img_r));
+		curandState *devStates;
+		curandGenerator_t gen;
+		cudaMalloc((void **)&devStates, 64*64*sizeof(curandState));
+		cudaMallocManaged((void **)&book, sizeof(book));
+		
+		double * means;
+		double * devs;
+		cudaMallocManaged(&means, generations*sizeof(double));
+		cudaMallocManaged(&devs, generations*sizeof(double));
 
-		// Copy data from host to device
-		cudaMemcpy(d_img_r, img.r, size * sizeof(*d_img_r), cudaMemcpyHostToDevice);
+		double volume1sec = volume24*1.0/(24*60*60);
 
-		// Bind pointers
-		cudaMemcpy(&(d_img->r), &d_img_r, sizeof(d_img->r), cudaMemcpyHostToDevice);
+		setup_kernel<<<ceil(1.0*generations/blocksize), blocksize>>>(devStates);
 
-		ppm * d_out;
-		int *d_out_r, *d_out_g, *d_out_b;
+		cudaEventRecord(start);
 
-		gpuErrchk(cudaMalloc((void**)&d_out, sizeof(ppm)));
-		gpuErrchk(cudaMemcpy(d_out, &out, sizeof(ppm), cudaMemcpyHostToDevice));
+		simulate_market<<<ceil(1.0*generations/blocksize), blocksize>>>(book, trades, devStates, means, devs);
 
-		cudaMalloc((void **) &d_out_r, size * sizeof(*d_out_r));
-		cudaMalloc((void **) &d_out_g, size * sizeof(*d_out_g));
-		cudaMalloc((void **) &d_out_b, size * sizeof(*d_out_b));
+		cudaEventRecord(stop);
+		cudaEventSynchronize(stop);
+		cudaEventElapsedTime(&total, start, stop);
 
-		// Copy data from host to device
-		cudaMemcpy(d_out_r, out.r, size * sizeof(*d_out_r), cudaMemcpyHostToDevice);
-		cudaMemcpy(d_out_g, out.g, size * sizeof(*d_out_g), cudaMemcpyHostToDevice);
-		cudaMemcpy(d_out_b, out.b, size * sizeof(*d_out_b), cudaMemcpyHostToDevice);
-
-		// Bind pointers
-		cudaMemcpy(&(d_out->r), &d_out_r, sizeof(d_out->r), cudaMemcpyHostToDevice);
-		cudaMemcpy(&(d_out->g), &d_out_g, sizeof(d_out->g), cudaMemcpyHostToDevice);
-		cudaMemcpy(&(d_out->b), &d_out_b, sizeof(d_out->b), cudaMemcpyHostToDevice);
-
-	// create GPU convo matrix
-		int * d_n;
-		int * d_cm;
-		gpuErrchk(cudaMalloc((void**)&d_n, 1*sizeof(int)));
-		gpuErrchk(cudaMemcpy(d_n, &n, 1*sizeof(int), cudaMemcpyHostToDevice));
-		gpuErrchk(cudaMalloc(&d_cm, sizeof(int)*n*n));
-		gpuErrchk(cudaMemcpy(d_cm, cm, sizeof(int)*n*n, cudaMemcpyHostToDevice));
-	// Launch Kernels
-		int blocksize = atoi(argv[4]);
-		int blockcount = (img.ysize/1.0/blocksize)+1;
-
-	// Start CPU timer and GPU timer
-		t.start();
-		gpuErrchk(cudaEventRecord(start));
-
-	// Actual kernel function
-		convolve_gpu<<< blockcount, blocksize >>>(d_img, d_out, d_n, d_cm, blockcount, blocksize);
-		getLastCudaError("Kernel execution failed (convolve_gpu).");
-
-	// Stop CPU and GPU timer
-		t.stop();
-		gpuErrchk(cudaEventRecord(stop));
-		gpuErrchk(cudaEventSynchronize(stop));
-
-	// save time to total float
-		gpuErrchk(cudaEventElapsedTime(&total, start, stop));
-		gpuErrchk(cudaMemcpy(&out, d_out, sizeof(ppm), cudaMemcpyDeviceToHost));
-
-	// COPY BACK TO CPU
-	//	int *out_r, *out_g, *out_b;
-	//	out_b = (int *) malloc(size * sizeof(int));
-	//	gpuErrchk(cudaMemcpy(out_b, d_out_b, size * sizeof(*out_b), cudaMemcpyDeviceToHost));
-	//	out.b = out_b;
-
-
-	// Print times
-		printf("%d, %d, %llu, %f, %s, %s\n", blocksize, blockcount, t.duration(), total, argv[2], argv[1]);
-	*/
-
+//		cudaDeviceSynchronize();
+		
+		
+		double avg_stddev = 0;
+		double avg_mean = 0;
+		for(int i=0; i<generations; i++) {
+//			printf("[%d] mean: %f, std-dev: %f\n", i, means[i], devs[i]);
+			avg_stddev += devs[i];
+			avg_mean += means[i];
+		}
+		avg_stddev = avg_stddev/generations;
+		avg_mean = avg_mean/generations;
+		
+		printf("[f] avg-mean: %f, avg-std-dev: %f\n", avg_mean, avg_stddev);
+		printf("[f] runtime: %f (ms), price-expected-in: %f (s)\n", total, trades*1.0/volume1sec);
+		
+	
 	// CUDA cleanup
 		gpuErrchk(cudaEventDestroy(start));
 		gpuErrchk(cudaEventDestroy(stop));
